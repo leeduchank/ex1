00ab3a965f7e441de8596deb50a4613aac9c30c2
KEYFU-thefighter <minhtricoding@gmail.com>
2021-10-14 11:57:57 +0700

    Ai làm câu nào thì ghi tên vô nhá

--- Source.cpp
+++ Source.cpp
@@ -4,8 +4,8 @@ using namespace std;
 //cam on da sua cai code. -nodatai- 
 void main()
 {	
-	cout << "Nhom 3\n" << "Cau 1: Le Duc Hanh\n" << "Cau 2: Chua ai lam\n" << "Cau 3: Truong Minh Tri\n\n";
-	//Cau 1 bat dau tu day -leeduchank-
+	cout << "Nhom 3\n" << "Cau 1:  Le Duc Hanh\n" << "Cau 2:  Chua ai lam\n" << "Cau 3:  Truong Minh Tri\n\n"; //Ai lam cau nao note vao di -nodatai-
+	//Cau 1 Bat dau tu day -leeduchank-
 	//Cau 1: Nhap nam sinh, xuat ra tuoi -leeduchank-
 	cout << "Cau 1: Nhap nam sinh, xuat ra tuoi\n";
 		int year;
@@ -13,24 +13,25 @@ void main()
 		cin >> year;
 		int age;
 		age = 2021 - year;
-	cout << " tuoi cua ban: " << age << endl << endl;
+		cout << " tuoi cua ban: " << age;
+	cout << "---------------------------" << endl << endl;
+
+	//Cau 2 Bat dau tu day	-chuabietailam-
+	//Cau 2: Nhap ky tu, xuat ra ky tu in hoa (cau nay ai lam v note vao di r sua ten tren cai nhom) -chuabietailam-
+	cout << "Cau 2: Nhap ky tu, xuat ra ky tu in hoa\n";
+		char ch;
+		cout<<"Enter a character in lowercase: "; 
+		cin>>ch;
+		if (ch <97)
+		{
+		cout << " Character has been in uppercase " ;
+		} 
+		else
+		ch=ch-32;
+		cout << "Entered character in uppercase: " << ch << endl;
 	cout << "---------------------------" << endl;
 
-	//Toi them 2 cai endl de no cach cau toi ra nha -nodatai-
-//cau 2 bat dau tu day	
-   char ch;
-   cout<<"Enter a character in lowercase: "; 
-   cin>>ch;
-   if (ch <97)
-   {
-   cout << " Character has been in uppercase " ;
-   } 
-   else
-   ch=ch-32;
-   cout<<"Entered character in uppercase: "<<ch<<endl
-   cout << "---------------------------" << endl;
-
-	//cau 3 bat dau tu day -nodatai-
+	//Cau 3 bat dau tu day -nodatai-
 	//Cau 3: nhap tien xuat ra to hop tien -nodatai-
 	cout << "Cau 3: Nhap vao so tien, xuat ra to hop cac loai tien \n";
 		int sotien=0;
@@ -45,6 +46,6 @@ void main()
 		cout << "5.000:   " << ((((((sotien % 500000) % 200000) % 100000) % 50000) % 20000) % 10000) / 5000 << endl;
 		cout << "2.000:   " << (((((((sotien % 500000) % 200000) % 100000) % 50000) % 20000) % 10000) % 5000) / 2000 << endl;
 		cout << "1.000:   " << ((((((((sotien % 500000) % 200000) % 100000) % 50000) % 20000) % 10000) % 5000) % 2000) / 1000 << endl;
-	        cout << "---------------------------" << endl;
+	cout << "---------------------------" << endl;
 return 0;
 }
